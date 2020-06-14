7081fbd9692a89fb45b150dd66baa2eb53a365dd
Christian Paranas <christiannparanas@gmail.com>
2020-06-14 23:19:45 +0800

    adjust some things and added pro sec

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-const texts = ['Programmer  ', 'Wannabe  ', 'Student  '];
+const texts = ['UX Designer  ', 'Programmer   ','Student   '];
 let count = 0;
 let index = 0;
 let currentText = '';
 let letter = '';
 
 // this code will envoke itself
-(function type() {
+function type() {
 
    if(count === texts.length){
       count = 0;
@@ -19,8 +19,46 @@ let letter = '';
       index = 0
    }
 
-   setTimeout(type, 400);
+   setTimeout(type, 250);
 
-}())
+}
+
+setTimeout(type, 3000)
+
+// nav
+
+const iconTrigger = document.querySelector('#iconTrigger')
+const icon = document.querySelector('#icon')
+const menuContainer = document.querySelector('.menu-container')
+
+iconTrigger.addEventListener('click', () => {
+   if (icon.classList.contains('fa-bars')) {
+
+      icon.classList.remove('fa-bars')
+      icon.classList.add('fa-close')
+
+      menuContainer.classList.add('show')
+      menuContainer.classList.remove('hide')
+
+   } else {
+
+      icon.classList.add('fa-bars')
+      icon.classList.remove('fa-close')
+
+      menuContainer.classList.remove('show')
+      menuContainer.classList.add('hide')
+
+   }
+
+})
+
+function parallax() {
+   icon.classList.add('fa-bars')
+   icon.classList.remove('fa-close')
+
+   menuContainer.classList.remove('show')
+   menuContainer.classList.add('hide')
+   
+ }
 
 
