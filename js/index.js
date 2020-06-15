8b6e1d218522f98d2dc7a80b45ba807d695a143f
Christian Paranas <christiannparanas@gmail.com>
2020-06-15 16:17:36 +0800

    added project section

--- js/index.js
+++ js/index.js
@@ -18,9 +18,7 @@ function type() {
       count++;
       index = 0
    }
-
    setTimeout(type, 250);
-
 }
 
 setTimeout(type, 3000)
@@ -33,23 +31,18 @@ const menuContainer = document.querySelector('.menu-container')
 
 iconTrigger.addEventListener('click', () => {
    if (icon.classList.contains('fa-bars')) {
-
       icon.classList.remove('fa-bars')
       icon.classList.add('fa-close')
 
       menuContainer.classList.add('show')
       menuContainer.classList.remove('hide')
-
    } else {
-
       icon.classList.add('fa-bars')
       icon.classList.remove('fa-close')
 
       menuContainer.classList.remove('show')
       menuContainer.classList.add('hide')
-
    }
-
 })
 
 function parallax() {
@@ -58,7 +51,6 @@ function parallax() {
 
    menuContainer.classList.remove('show')
    menuContainer.classList.add('hide')
-   
  }
 
 
