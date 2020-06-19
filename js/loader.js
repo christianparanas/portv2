858df02d52c63c86254f51234ebe4df893543041
Christian Paranas <christiannparanas@gmail.com>
2020-06-19 14:37:45 +0800

    added loader

---
+++ js/loader.js
@@ -0,0 +1,12 @@
+
+$(window).on("load", function() {
+   $(".loader").fadeOut("slow");
+
+   $("#head").show();
+   $(".pro").show();
+   $(".skills").show();
+   $(".projects").show();
+   $(".contact").show();
+   $("#foot").show();
+   
+})
